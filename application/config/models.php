58ac6d9fa2fd15a4fda6e8a26a2c4aa35e87cdcf
alevesque <arthur2330.al@gmail.com>
2021-01-26 16:09:17 +0100

    Dossier Master

---
+++ application/config/models.php
@@ -0,0 +1,14 @@
+<?php
+/*
+| -------------------------------------------------------------------
+| Fichier de configuration des modèles de l'application
+| -------------------------------------------------------------------
+| Ce fichier regroupe les paramètres d'accès à la source de données
+|
+*/
+$config['hostname'] = 'localhost';
+$config['username'] = 'userGsb';
+$config['password'] = 'secret';
+$config['database'] = 'gsbfrais';
+$config['dbdriver'] = 'mysql';
+?>
