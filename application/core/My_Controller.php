07c8b9ff6eaa908408d29c42b761e16ed15bc6d2
Simon Tessard <simon.tessard@gmail.com>
2021-03-27 23:56:00 +0100

    Added basic authentification for visitor (All rights granted)

--- application/core/My_Controller.php
+++ application/core/My_Controller.php
@@ -40,25 +40,25 @@ abstract class My_Controller extends CI_Controller {
       $password = $chaineadiviser[1];
     }
     
-
     $tab = $this->mVisiteur->getByCredentials($login,$password);
 
-      /// Si le login et le mot de passe se trouvent dans la base et correspondent alors:
-      if ($tab != FALSE)
-      {
-        $response = ["status" => "OK", "data" => $tab];
-        $json = json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    // Si le mot de passe est vide
+    if (empty($password))
+    {
+      $response = ["status" => "NO", "data" => "Accès refusé - mot de passe vide"];
+      $json = json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
-        $this->output
-        ->set_status_header(200)
-        ->set_content_type('application/json','utf-8')
-        ->set_output(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)) 
-        ->_display();
-        die();
-      }
-      elseif (empty($password))
+      $this->output
+      ->set_status_header(403)
+      ->set_content_type('application/json','utf-8')
+      ->set_output(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)) 
+      ->_display();
+      die();
+    } 
+      /// Si le login et le mot de passe se trouvent dans la base et correspondent alors:
+      elseif ($tab == FALSE)
       {
-        $response = ["status" => "NO", "data" => "Accès refusé - identifiant ou mot de passe vide "];
+        $response = ["status" => "NO", "data" => "Accès refusé - Identifiant et/ou mot de passe invalide"];
         $json = json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
         $this->output
@@ -68,19 +68,6 @@ abstract class My_Controller extends CI_Controller {
         ->_display();
         die();
       }
-      /* 
-      else 
-      {
-        $response = ["status" => "NO", "data" => "Accès refusé - invalide"];
-        $json = json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-
-        $this->output
-        ->set_status_header(403)
-        ->set_content_type('application/json','utf-8')
-        ->set_output(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)) 
-        ->_display();
-        die();
-      } */
   }
 }
 
