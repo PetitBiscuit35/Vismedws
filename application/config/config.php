ebbf90e0ff7428cccba6032f78bbb07a0fd6fdce
TESSARD <simon.tessard@gmail.com>
2021-03-31 16:46:13 +0200

    Config changed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://localhost/sio2/simon/ppe31_vismedws_gr2';
+$config['base_url'] = 'https://localhost/SIO2/TESSARD/WS-VisMed';
 /*
 |--------------------------------------------------------------------------
 | Index File
